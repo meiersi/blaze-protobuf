c514d88f7a87823351366fe682be8d1e90982148
Simon Meier <iridcode@gmail.com>
2011-12-16 23:01:09 +0100

    further benchmarks - achieving Java speed seems possible

--- BaseLine.hs
+++ BaseLine.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# LANGUAGE OverloadedStrings, FlexibleInstances, TypeSynonymInstances #-}
 -- | Copyright: Simon Meier <iridcode@gmail.com>
 --
 -- Baseline estimate of serialization speed.
@@ -63,8 +63,8 @@ data Size = SMALL | LARGE
        deriving( Enum )
 
 data Image = Image 
-       String                    -- 1: url to the thumbnail
-       (Maybe String)            -- 2: url in the html ALT
+       S.ByteString                    -- 1: url to the thumbnail
+       (Maybe S.ByteString)            -- 2: url in the html ALT
        {-# UNPACK #-} !Int32     -- 3: width of the image
        {-# UNPACK #-} !Int32     -- 4: height of the image
        Size                      -- 5: size of the image (in relative terms)
@@ -73,17 +73,17 @@ data Player = JAVA | FLASH
         deriving( Enum )
        
 data Media = Media
-       String                  -- 1: uri to the video, may not be an actual URL
-       (Maybe String)          -- 2: title used in the html ALT
+       S.ByteString                  -- 1: uri to the video, may not be an actual URL
+       (Maybe S.ByteString)          -- 2: title used in the html ALT
        {-# UNPACK #-} !Int32   -- 3: width of the video
        {-# UNPACK #-} !Int32   -- 4: height of the video
-       String                  -- 5: format: avi, jpg, youtube, cnbc, audio/mpeg formats ...
+       S.ByteString                  -- 5: format: avi, jpg, youtube, cnbc, audio/mpeg formats ...
        {-# UNPACK #-} !Int64   -- 6: duration time in miliseconds
        {-# UNPACK #-} !Int64   -- 7: file size
        (Maybe Int32)           -- 8: bitrate of the video 
-       [String]                -- 9: names of people featured in the video
+       [S.ByteString]                -- 9: names of people featured in the video
        Player                  -- 10: in case of a player specific media
-       (Maybe String)          -- 11: media copyright
+       (Maybe S.ByteString)          -- 11: media copyright
 
 data MediaContent = MediaContent
        [Image]        -- 1: images
@@ -141,11 +141,16 @@ testValue = MediaContent
 -- Manual encoding in custom binary format (similar to the 'binary' library)
 ----------------------------------------------------------------------------
 
+{-
 binString :: String -> Builder
 binString cs = 
     E.encodeListWithB taggedCharUtf8 cs <> word8 0
   where
     taggedCharUtf8 = (\c -> (1, c)) >$< fromF E.word8 `pairB` E.charUtf8
+-}
+binString :: S.ByteString -> Builder
+binString bs = 
+    intHost (S.length bs) <> byteString bs
 
 binSize :: Size -> Builder
 binSize = word8 . fromIntegral . fromEnum
@@ -154,7 +159,7 @@ binMaybe :: (a -> Builder) -> Maybe a -> Builder
 binMaybe _ Nothing  = word8 0
 binMaybe f (Just x) = word8 1 <> f x
 
-binMayString :: Maybe String -> Builder
+binMayString :: Maybe S.ByteString -> Builder
 binMayString = binMaybe binString
 
 binList :: (a -> Builder) -> [a] -> Builder
@@ -197,7 +202,7 @@ data Field =
        -- | FInt32   {-# UNPACK #-} !Int32   -- ^ fixed-size int32
        -- | FInt64   {-# UNPACK #-} !Int64   -- ^ fixed-size int64
        -- | Double   {-# UNPACK #-} !Double  -- ^ A 'Double' value.
-       | String   String                  -- TODO: Use 'text' package
+       | String   S.ByteString                  -- TODO: Use 'text' package
        -- | Bytes    S.ByteString
        | Message  Msg
        -- TODO: use vector package for repeated fields.
@@ -209,7 +214,7 @@ data Field =
        -- | FInt64V  [Int64]
        -- | DoubleV  [Double]
        -- | BoolV    [Bool]
-       | StringV  [String]
+       | StringV  [S.ByteString]
        | MessageV [Msg]
          -- ^ Embedded message TODO: allow caching via bytestring
        deriving( Eq, Ord, Show )
@@ -226,32 +231,136 @@ encodeWithVarLen :: Builder -> Builder
 encodeWithVarLen = 
     E.encodeWithSize (fromIntegral defaultChunkSize) E.word64VarFixedBound
 
+{-# INLINE encodeTag #-}
+encodeTag :: E.BoundedEncoding Tag
+encodeTag = ifB (< 0x7f) (fromIntegral >$< fromF E.word8) E.intVar
+
+{- Not working out
+encodeLen :: Word64 -> E.FixedEncoding Word64
+encodeLen bound
+  | bound < 0x7f = fromIntegral >$< E.word8
+  | otherwise    = E.word64VarFixedBound bound
+-}
+
 renderField :: Tag -> Field -> Builder
 renderField tag (Int32 i) = 
     E.encodeWithB encInt32 (fromIntegral tag, i)
   where
-    encInt32 = E.int32Var `pairB` E.int32Var
+    encInt32 = encodeTag `pairB` E.int32Var
 renderField tag (Int64 i) =
     E.encodeWithB encInt64 (fromIntegral tag, i)
   where
-    encInt64 = E.int32Var `pairB` E.int64Var
+    encInt64 = encodeTag `pairB` E.int64Var
 renderField tag (String cs)     = renderTaggedString  tag cs
 renderField tag (Message msg)   = renderTaggedMessage tag msg
 renderField tag (StringV css)   = foldMap (renderTaggedString  tag) css
 renderField tag (MessageV msgs) = foldMap (renderTaggedMessage tag) msgs
 
+renderTaggedString :: Tag -> S.ByteString -> Builder
+renderTaggedString tag bs = 
+    E.encodeWithB encodeTag tag <> E.encodeWithB E.intVar (S.length bs) <>
+    byteString bs
+
+{-
 renderTaggedString :: Tag -> String -> Builder
 renderTaggedString tag cs = 
     E.encodeWithB E.int32Var (fromIntegral tag) <>
     encodeWithVarLen (stringUtf8 cs)
+-}
 
 renderTaggedMessage :: Tag -> Msg -> Builder
 renderTaggedMessage tag msg = 
-    E.encodeWithB E.int32Var (fromIntegral tag) <>
+    E.encodeWithB encodeTag tag <>
     encodeWithVarLen (renderMessage msg)
 
 renderMessage :: Msg -> Builder
 renderMessage = foldMap (uncurry renderField) . IM.toAscList . getMsg
+
+-- Direct rendering
+-------------------------------------------
+
+pbInt32 :: Int32 -> Builder
+pbInt32 = E.encodeWithB E.int32Var
+
+pbInt64 :: Int64 -> Builder
+pbInt64 = E.encodeWithB E.int64Var
+
+pbSize :: Size -> Builder
+pbSize = pbInt32 . fromIntegral . fromEnum
+
+pbPlayer :: Player -> Builder
+pbPlayer = pbInt32 . fromIntegral . fromEnum
+
+-- | Tag a value together with its field and wire type.
+pbTag :: Int -> FieldId -> Builder
+pbTag ty fid = E.encodeWithB encodeTag $ (fromIntegral fid `shiftL` 3) .|. ty
+
+pbLDelim :: FieldId -> Builder
+pbLDelim = pbTag 2
+
+pbVar :: FieldId -> Builder
+pbVar = pbTag 0
+
+pbString :: S.ByteString -> Builder
+pbString bs = 
+    E.encodeWithB E.intVar (S.length bs) <> byteString bs
+    -- encodeWithVarLen $ byteString bs
+    -- OUCH: encodeWithVarLen accounts for one third of the runtime
+
+pbOptional :: (a -> Builder) -> Maybe a -> Builder
+pbOptional = maybe mempty
+
+pbImage :: Image -> Builder
+pbImage (Image uri title width height size) = -- encodeWithVarLen $
+                  pbLDelim 1 <>    pbString  uri
+  <> pbOptional ((pbLDelim 2 <>) . pbString) title
+  <>              pbVar    3 <>    pbInt32   width
+  <>              pbVar    4 <>    pbInt32   height
+  <>              pbVar    5 <>    pbSize    size
+
+pbMedia :: Media -> Builder
+pbMedia (Media uri title width height format duration size rate 
+                   people player copy) = -- encodeWithVarLen $
+                  pbLDelim  1 <>    pbString  uri
+  <> pbOptional ((pbLDelim  2 <>) . pbString) title
+  <>              pbVar     3 <>    pbInt32   width
+  <>              pbVar     4 <>    pbInt32   height
+  <>              pbLDelim  5 <>    pbString  format
+  <>              pbVar     6 <>    pbInt64   duration
+  <>              pbVar     7 <>    pbInt64   size
+  <> pbOptional ((pbVar     8 <>) . pbInt32)  rate
+  <> foldMap    ((pbLDelim  9 <>) . pbString) people
+  <>              pbVar    10 <>    pbPlayer  player
+  <> pbOptional ((pbLDelim 11 <>) . pbString) copy
+
+pbMediaContent :: MediaContent -> Builder
+pbMediaContent (MediaContent images media) = -- encodeWithVarLen $
+     foldMap    ((pbLDelim  1 <>) . pbImage) images
+  <>              pbLDelim  2 <>    pbMedia  media
+
+-- NOTE: The direct conversion to 'Builder' have the desired speed. However,
+-- we probably want the content of length-delimited fields to be given as
+-- 'Builder's to allow their dynamic generation.
+
+{-
+-- Something as follows: Polymorphic fields that can be converted to their
+-- parsed version and output version
+--
+data PersonGen firstName surName = 
+       Person { 
+           firstName :: firstName  -- field 1
+         , surName   :: surName    -- field 2
+         }
+
+type PersonIn  = Person S.ByteString S.ByteString
+type PersonOut = Person Builder Builder
+
+pbPerson :: PersonOut
+pbPerson = encodeWithVarLen
+
+Hmm..not sure.
+
+-}
 
 -- Internal message construction functions.
 -------------------------------------------
@@ -292,10 +401,10 @@ instance ToField Int32 where
 instance ToField Int64 where
     toField = Int64
 
-instance ToField String where
+instance ToField S.ByteString where
     toField = String
 
-instance ToField [String] where
+instance ToField [S.ByteString] where
     toField = StringV
 
 instance ToField Msg where
@@ -357,17 +466,18 @@ imageMsg (Image uri title width height size) = Msg $
       , size   `asVar`    5
       ]
 
+------------------------------------------------------------------------------
+-- Variable length encoding
+------------------------------------------------------------------------------
 
--- A draft of the actual interface
-----------------------------------
+oneWord8VarLen :: Word8 -> Builder
+oneWord8VarLen = encodeWithVarLen . word8
 
--- One type per message type.
--- One fclabels accessor per required field.
--- x = Image.title becomes
--- get imageTitle img
-
-
-
+oneWord8VarLenBase :: Word8 -> Builder
+oneWord8VarLenBase w = 
+    (E.encodeWithF (E.word64VarFixedBound w') w') <> word8 w
+  where
+    w' = fromIntegral w
 
 ------------------------------------------------------------------------------
 -- Benchmarking code
@@ -375,16 +485,33 @@ imageMsg (Image uri title width height size) = Msg $
 
 testValues = replicate 100 testValue
 
+word8s :: [Word8]
+word8s = take 100 $ cycle [0..]
+
 main :: IO ()
 main = defaultMain
-  [ bench "100 protobuf/generic" $ whnf 
-        (L.length . toLazyByteString . foldMap (renderMessage . mediaContentMsg)) testValues
-  , bench "100 bin/manual" $ whnf 
-        (L.length . toLazyByteString . foldMap (binMediaContent)) testValues
-  , bench "protobuf/generic" $ whnf 
+  -- Benchmark 100 times word8s to remove influence of buffer allocation
+  -- negligible.
+  [ bench "oneWord8VarLen - baseline" $ whnf 
+        (L.length . toLazyByteString . foldMap oneWord8VarLenBase) word8s
+
+  , bench "oneWord8VarLen" $ whnf 
+        (L.length . toLazyByteString . foldMap oneWord8VarLen) word8s
+
+  , bench "protobuf/manual" $ whnf 
+        (L.length . toLazyByteString . pbMediaContent) testValue
+
+  , bench "protobuf/generic with conversion" $ whnf 
         (L.length . toLazyByteString . renderMessage . mediaContentMsg) testValue
-  , bench "bin/manual" $ whnf 
+
+  , bench "protobuf/generic no conversion" $ whnf 
+        (L.length . toLazyByteString . renderMessage) (mediaContentMsg testValue)
+
+  , bench "bin/manual: default settings" $ whnf 
         (L.length . toLazyByteString . binMediaContent) testValue
+
+  , bench "bin/manual: short first buffer, no trimming" $ whnf 
+        (L.length . toLazyByteStringWith (untrimmedStrategy 512 defaultChunkSize) L.empty . binMediaContent) testValue
   ]
 
 
